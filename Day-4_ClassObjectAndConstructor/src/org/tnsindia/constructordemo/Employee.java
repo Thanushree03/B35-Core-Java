d936e86d1165c52d703f573beaeb1ae69f48353a
Dell <Dell@DESKTOP-A8RANKN>
2022-11-24 13:07:46 +0530

    first commit

---
+++ Day-4_ClassObjectAndConstructor/src/org/tnsindia/constructordemo/Employee.java
@@ -0,0 +1,35 @@
+package org.tnsindia.constructordemo;
+
+public class Employee {
+
+	
+		public int salary;
+		public String name;
+		//default constructor
+		Employee()
+		{
+			System.out.println("Default constructor");
+		}
+		//parameterized constructor
+		public Employee(int salary, String name) {
+			super();
+			this.salary = salary;
+			this.name = name;
+		
+		  salary=25000;
+		  name="nikitha";
+		  
+		}
+		
+		
+		//method
+		public void print()
+		{
+			System.out.println("The salary of "+name+" is: "+salary);
+		}
+		
+		}
+
+	
+
+
