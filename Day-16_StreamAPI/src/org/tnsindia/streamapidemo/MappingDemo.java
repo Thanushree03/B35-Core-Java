73b42c3ec17ba3df606795875549b9f0ea72b770
Dell <Dell@DESKTOP-A8RANKN>
2022-12-14 19:37:01 +0530

    first commit

---
+++ Day-16_StreamAPI/src/org/tnsindia/streamapidemo/MappingDemo.java
@@ -0,0 +1,13 @@
+package org.tnsindia.streamapidemo;
+
+import java.util.Arrays;
+import java.util.List;
+//Example on Map() operation
+public class MappingDemo {
+
+	public static void main(String[] args) {
+		List<String>obj1=Arrays.asList(new String[]{"Thanu","Bhavya","Uday"});
+		obj1.stream().map(i->i.length()).forEach((i)->System.out.print(i+ " "));
+	}
+
+}
